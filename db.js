7eeb40354b9ee65f2bde15e78dd02b6ed3183239
nilcrepa-ops <nilcrepa@gmail.com>
2026-04-27 10:59:29 +0200

    Cosas configuradas

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv').config({ path: require('path').resolve(__dirname, '.env') });
 
 //conexion con BBDD - TODO: Usar usuario creado en phpmyadmin y .env para credenciales
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'apiexpress'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
+
 
 connection.connect(error => {
     if(error) throw error;
